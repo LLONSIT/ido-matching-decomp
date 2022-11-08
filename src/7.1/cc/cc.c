253cc23dcf1a387fb5c0b592cf7029c01b239ed4
Angie <angheloalf95@gmail.com>
2022-11-08 18:41:18 -0300

    preparations for native builds

--- src/7.1/cc/cc.c
+++ src/7.1/cc/cc.c
@@ -6,23 +6,32 @@
 // function _mcount # 1
 // #pragma GLOBAL_ASM("asm/7.1/functions/cc/_mcount.s")
 
-#include "signal.h"
-#include "wait.h"
-#include "sex.h"
-#include "sys/times.h"
-#include "stdlib.h"
-#include "utime.h"
-#include "varargs.h"
-#include "errno.h"
-#include "unistd.h"
-#include "ctype.h"
-#include "sys/stat.h"
-#include "sys/fcntl.h"
-#include "sys/ioctl.h"
-#include "ucontext.h"
+// Headers which should be available in modern systems
+#include <string.h>
+#include <malloc.h>
+#include <signal.h>
+#include <wait.h>
+#include <sys/times.h>
+#include <stdlib.h>
+#include <utime.h>
+#include <errno.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+#include <sys/ioctl.h>
+#include <ucontext.h>
+
+#ifdef NATIVE_BUILD
+#include <stdarg.h>
+#else
+#include "indy/varargs.h"
+#endif
+
+// indy specific headers
+struct ranlib; // avoids errors on native builds
+#include "indy/sex.h"
 #include "sys/procfs.h"
-#include "string.h"
-#include "malloc.h"
 
 #ifndef TRUE
 #define TRUE 1
